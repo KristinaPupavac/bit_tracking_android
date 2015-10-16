b1de6c4ab436d9d8735f8657a41b6b98bd8288f5
KristinaPupavac <kristina.pupavac@bitcamp.ba>
2015-10-16 15:17:03 +0200

    initila commit

---
+++ BitTrackingApplication/app/src/main/java/ba/bitcamp/bittracking/bittrackingapplication/BitTrackingActivity.java
@@ -0,0 +1,13 @@
+package ba.bitcamp.bittracking.bittrackingapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BitTrackingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bit_tracking);
+    }
+}
