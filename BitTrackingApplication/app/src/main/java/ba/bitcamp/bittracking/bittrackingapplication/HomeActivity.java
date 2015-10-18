804bf539124643331deda131de4ddb4243988a9f
KristinaPupavac <kristina.pupavac@bitcamp.ba>
2015-10-18 15:50:07 +0200

    package status and sign out

--- BitTrackingApplication/app/src/main/java/ba/bitcamp/bittracking/bittrackingapplication/HomeActivity.java
+++ BitTrackingApplication/app/src/main/java/ba/bitcamp/bittracking/bittrackingapplication/HomeActivity.java
@@ -1,16 +1,86 @@
 package ba.bitcamp.bittracking.bittrackingapplication;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Kristina Pupavac on 10/17/2015.
  */
 public class HomeActivity extends AppCompatActivity {
+    public  List<Package> list = new LinkedList<Package>();
+    Package p1 = new Package(Package.READY_FOR_SHIPPING );
+    Package p2 = new Package(Package.ON_ROUTE);
+    Package p3 = new Package(Package.OUT_FOR_DELIVERY);
+    Package p4 = new Package(Package.DELIVERED);
+    Package p5 = new Package(Package.RECEIVED);
+
+    private EditText mTruckingNumber;
+    private Button mButton;
+    private Button mSignOut;
+    private LinearLayout mLinearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        list.add(p1);
+        list.add(p2);
+        list.add(p3);
+        list.add(p4);
+        list.add(p5);
+
+        mTruckingNumber = (EditText) findViewById(R.id.trucking_number);
+        mButton = (Button) findViewById(R.id.button);
+        mLinearLayout = (LinearLayout) findViewById(R.id.layout);
+        mSignOut = (Button) findViewById(R.id.sign_out);
+
+        TextView text = new TextView(HomeActivity.this);
+        text.setText("Tracking package numbers:  p1: " + p1.getToken() + " p2: " +p2.getToken()
+                + " p3: " +p3.getToken()+ " p4: " +p4.getToken()+ " p5: " +p5.getToken());
+        mLinearLayout.addView(text);
+
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < list.size(); i++) {
+                    if (list.get(i).getToken().toString().equals(mTruckingNumber.getText().toString())) {
+                        TextView t = new TextView(HomeActivity.this);
+                        t.setText("Package status: " + list.get(i).getStatus());
+                        mLinearLayout.addView(t);
+                    } else {
+                        mTruckingNumber.setError("Wrong trucking number!");
+                    }
+                }
+
+            }
+        });
+
+
+        mSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(HomeActivity.this, BitTrackingActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
 
     }
--- BitTrackingApplication/app/src/main/java/ba/bitcamp/bittracking/bittrackingapplication/Package.java
+++ BitTrackingApplication/app/src/main/java/ba/bitcamp/bittracking/bittrackingapplication/Package.java
@@ -1,8 +1,42 @@
 package ba.bitcamp.bittracking.bittrackingapplication;
 
+import java.util.UUID;
+
 /**
  * Created by Kristina Pupavac on 10/18/2015.
  */
 public class Package {
-    
+    public static final int READY_FOR_SHIPPING = 1;
+    public static  final int ON_ROUTE = 2;
+    public static final int OUT_FOR_DELIVERY = 3;
+    public static final int DELIVERED = 4;
+    public static final int RECEIVED = 5;
+
+    private UUID token;
+    private int status;
+
+    public Package (){
+
+    }
+
+    public Package(int status){
+        this.token =  UUID.randomUUID();
+        this.status = status;
+    }
+
+    public UUID getToken() {
+        return token;
+    }
+
+    public void setToken(UUID token) {
+        this.token = token;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
