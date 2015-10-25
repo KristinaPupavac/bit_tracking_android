9b974c7c1672626f8136fbffd882066f23ed39eb
KristinaPupavac <kristina.pupavac@bitcamp.ba>
2015-10-25 20:47:08 +0100

    twiter and facebook buttons links

--- BitTrackingApplication/app/src/main/java/ba/bitcamp/bittracking/bittrackingapplication/controllers/BitTrackingActivity.java
+++ BitTrackingApplication/app/src/main/java/ba/bitcamp/bittracking/bittrackingapplication/controllers/BitTrackingActivity.java
@@ -1,6 +1,7 @@
 package ba.bitcamp.bittracking.bittrackingapplication.controllers;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import org.json.JSONException;
@@ -28,6 +30,12 @@ import ba.bitcamp.bittracking.bittrackingapplication.models.User;
 public class BitTrackingActivity extends AppCompatActivity {
     private Button mLoginButton;
     private Button mRegister;
+    private ImageButton mFacebook;
+    private ImageButton mTwitter;
+    private ImageButton mLocation;
+    private ImageButton mInfo;
+
+
 
     private EditText mMail;
     private EditText mPassword;
@@ -41,10 +49,12 @@ public class BitTrackingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         mLoginButton = (Button) findViewById(R.id.loginbtn);
-
         mRegister = (Button) findViewById(R.id.register);
-
-
+        mFacebook = (ImageButton) findViewById(R.id.facebook_button);
+        mTwitter = (ImageButton) findViewById(R.id.twitter_button);
+        mLocation = (ImageButton) findViewById(R.id.location_button);
+        mInfo = (ImageButton) findViewById(R.id.info_button);
+        
         mMail = (EditText) findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
 
@@ -73,6 +83,25 @@ public class BitTrackingActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(BitTrackingActivity.this, RegisterActivity.class));
+            }
+        });
+
+        mFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("https://www.facebook.com/Bit-Tracking-462032430635765/"));
+                startActivity(intent);
+            }
+        });
+
+        mTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("https://twitter.com/BitTracking"));
+                startActivity(intent);
+
             }
         });
 
